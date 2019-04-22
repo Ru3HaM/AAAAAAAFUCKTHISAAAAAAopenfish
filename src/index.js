b3962f7aa9cdd026e3ceccb6eb94dc7eaddcb281
Ru3HaM <dudko.dan@yandex.ru>
2019-04-22 21:07:06 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './assets/styles/main.scss';
+import Menu from './assets/styles/components/Menu.jsx';
+import MainMap from './assets/styles/components/MainMap.jsx';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
